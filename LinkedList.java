627df3e947e18a7b59c2144ae3eb2a3050ccd54a
Ali Alhoshan <143254937+A-Alhoshan@users.noreply.github.com>
2023-10-13 17:58:36 +0300

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 public class LinkedList<T> {
     //Atrr
-    private Node<T> head;
-    private Node<T> current;
-    private Node<T> tail;
+    private Node<Contact> head;
+    private Node<Contact> current;
+    private Node<Contact> tail;
 
     //Constructor and DLL methods
     public LinkedList() {
@@ -33,6 +35,7 @@ public class LinkedList<T> {
 	}
     //هذي تشيك لو كان الكونتاكت موجود أو لا (ترجع خطأ لو كان مب موجود وصح لو موجود)
     public boolean alreadyExist(Contact contact){
+      
         boolean flag = false;
         if(empty()){
             return flag;
@@ -40,8 +43,8 @@ public class LinkedList<T> {
         else {
             findFirst();
             while(current !=null){
-            if(this.current.contact.getName()==contact.getName()
-            && this.current.contact.getPhoneNumber()==contact.getPhoneNumber()){
+            if(this.current.contact.getName().equalsIgnoreCase(contact.getName()) //نفس الاسم بالضبط بدون اعتبار للكبتل والسمول
+            && this.current.contact.getPhoneNumber()==contact.getPhoneNumber()){  //نفس الرقم بالضبط
                 flag = true;
             }
             findNext();
@@ -53,11 +56,11 @@ public class LinkedList<T> {
 
     public void addContact(Contact contact){
         if(alreadyExist(contact)){
-            System.out.println("Contact "+contact.getName()+" is already exist");
+            System.out.println("Contact "+contact.getName()+" is already exist!");
         }
         else{//not exist
 
-            Node<T> newNode = new Node<T>(contact);
+            Node<Contact> newNode = new Node<Contact>(contact);
             if (empty()){
                 head = newNode;
             }       
@@ -92,6 +95,64 @@ public class LinkedList<T> {
 
 
 
-    //Search
+    //Unique search  نبحث بالأشياء الي ماتتكرر زي الايميل ورقم الجوال باختصار ضامنين نرجع اوبجكت واحد
+    //لاتنسَ هذي النوعية من السيرش بنخليها ترجع اوبجكت مب طباعة يعني لو بتستدعيها بالمين لاتنسَ تطبعها من هناك  
+        public Node<Contact> searchByEmailAddress(String email){
+            findFirst();
+            while(current!=null){
+                if(current.contact.getEmailAddress().equalsIgnoreCase(email)){
+                    return current;
+                }
+                findNext();
+            }
+            return null;
 
+        }
+        public Node<Contact> searchByPhoneNumber(String num){
+            findFirst();
+            while(current!=null){
+                if(current.contact.getPhoneNumber().equals(num)){
+                    return current;
+                }
+                findNext();
+            }
+            return null;
+        }
+        //not unique search الي قد يكون فيها تكرار نفس الاسم والآدريس ويوم الميلاد
+        //هذي قد ترجع لك أكثر من شخص وش الحل؟ نخليهاترجع لك مصفوفةاوبجيكتات
+        //الثلاث ميثودات هذولي يرجعون إما فارغة او اوبجكت او اوبجكتس لاتنسَ تتعامل مع ذا الشيء
+        public List<Contact> searchByName(String name){
+            List<Contact> foundContact_s = new ArrayList<>();
+            findFirst();
+            while(current!=null){
+                if(current.contact.getName().toString().toLowerCase().contains(name.toLowerCase())){
+                    foundContact_s.add(current.contact);
+                }
+                findNext();
+            }
+            return foundContact_s;
+        }
+        public List<Contact> searchByAddress(String address){
+            List<Contact> foundContact_s = new ArrayList<>();
+            findFirst();
+            while(current!=null){
+                if(current.contact.getAddress().toString().toLowerCase().contains(address.toLowerCase())){
+                foundContact_s.add(current.contact);
+                }
+                findNext();
+            }
+            return foundContact_s;
+        }
+        public List<Contact> searchByBirthday(String birthday){
+            List<Contact> foundContact_s = new ArrayList<>();
+            findFirst();
+            while(current!=null){
+                if(current.contact.getBirthDay().equalsIgnoreCase(birthday)){
+                    foundContact_s.add(current.contact);
+                }
+                findNext();
+            }
+            return foundContact_s;
+        }
+        //delete...
 }
