3b74f0a956745690a9680adcc8377e9af73f26fb
Ali Alhoshan <143254937+A-Alhoshan@users.noreply.github.com>
2023-10-13 20:45:52 +0300

    Update Event.java

--- Event.java
+++ Event.java
@@ -2,23 +2,27 @@ public class Event implements Comparable<Event> {
     		
 		//Attributes
 	    private String title;
-	    private String date;
-		private String time;
+	    private String date; //(MM/DD/YYYY) AS Mentioned in requirement
+		private String startTime;//HH:mm 
+		private String endTime; //HH:mm هذولي الثنين قسمناهم عشان نعرف اذا فيه تعارض بين ايفنتين ولالا
 	    private String location;
 	    private Contact contact; //هنا حطينا متغير كونتاكت من نوع كونتاكت عشان بنضيف الفعاليات للكونتاكت نفسه فبنحتاجه في هذا الكلاس
-	  
+	    						 //We will assume that an event have only one contact
 	    //Default and Set Constructors 
 	    public Event() {
 	    	this.title = null;
 	    	this.date=null;
 	    	this.location=null;
 	    	this.contact=null;
-			this.time=null;
+			this.startTime=null;
+			this.endTime=null;
 	    }
-	    public Event(String title, String date,String time, String location, Contact contact) {
+	    public Event(String title, String date,String startTime,String endTime,
+					 String location, Contact contact) {
 	        this.title = title;
 	        this.date = date;
-			this.time=time;
+			this.startTime=startTime;
+			this.endTime=endTime;
 	        this.location = location;
 	        this.contact = contact;
 	    }
@@ -35,17 +39,21 @@ public class Event implements Comparable<Event> {
 	    public String getDate() {
 	        return date;
 	    }
-		public String getTime(){
-			return time;
-		}
-
-	    public void setDate(String date) {
+		public void setDate(String date) {
 	        this.date = date;
 	    }
-		public void setTime(String time){
-			this.time=time;
+		public String getstartTime(){
+			return startTime;
 		}
-
+		public void setstartTime(String startTime){
+			this.startTime=startTime;
+		}
+		public String getendTime(){
+			return endTime;
+		}
+		public void setendTime(String endTime){
+			this.endTime=endTime;
+		}
 	    public String getLocation() {
 	        return location;
 	    }
@@ -74,7 +82,8 @@ public class Event implements Comparable<Event> {
 			System.out.println("Event title:"+this.title);
 			System.out.println("Contact name:"+this.contact.getName());
 			System.out.println("Event date:"+this.date);
-			System.out.println("Event time:"+this.time);
+			System.out.println("Event Start time"+this.startTime);
+			System.out.println("Event Start time"+this.endTime);
 			System.out.println("Event location"+this.location);
 		}
 	}
