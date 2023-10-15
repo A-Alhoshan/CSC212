facd4eff0b6d0b937eb252e637eaec9a2aa25496
Ziyad-Alshareef <147332654+Ziyad-Alshareef@users.noreply.github.com>
2023-10-15 15:12:24 +0300

    Update LinkedListN.java

--- LinkedListN.java
+++ LinkedListN.java
@@ -8,7 +8,7 @@ public class LinkedListN {
     
  // Constructor and DLL methods
  	public LinkedListN() {
- 		head = current = null;
+ 		head = current = tail = null;
  	}
 
  	public boolean empty() {
