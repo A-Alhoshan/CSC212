60fed631f6b1d43c916a6ee62df8ee230b58658f
Ali Alhoshan <143254937+A-Alhoshan@users.noreply.github.com>
2023-10-15 18:23:41 +0300

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,17 +1,12 @@
 public class Node<T> {
-    public Contact contact;
-    public Event event;
     public Node<T> prev;
     public Node<T> next;
-    
+    public T data;
     public Node(){
         this.prev=null;
         this.next=null;
     }
-    public Node(Contact c){
-     this.contact = c;
-    }
-    public Node(Event e){
-       this.event=e;
+    public Node(T t){
+     this.data = t;
     }
 }
