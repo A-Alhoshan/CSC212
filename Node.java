2fd0098305a7316b220d59a1cfa36195ebd7fb29
Ali Alhoshan <143254937+A-Alhoshan@users.noreply.github.com>
2023-12-03 23:22:53 +0300

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,12 +1,14 @@
 public class Node<T> {
-    public Node<T> prev;
-    public Node<T> next;
-    public T data;
-    public Node(){
-        this.prev=null;
-        this.next=null;
-    }
-    public Node(T t){
-     this.data = t;
-    }
+	public Node<T> prev;
+	public Node<T> next;
+	public T data;
+
+	public Node() {
+		this.prev = null;
+		this.next = null;
+	}
+
+	public Node(T t) {
+		this.data = t;
+	}
 }
